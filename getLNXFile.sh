32a1dc092683c169763542bc27392690ae651cce
Theblackthreat <57785831+theblackthreat@users.noreply.github.com>
2021-07-09 10:27:25 +0530

    Update getLNXFile.sh

--- getLNXFile.sh
+++ getLNXFile.sh
@@ -9,3 +9,11 @@ find / -user [Primaery_user] -group [group_name]  -size [size]c -type f 2>/dev/n
 find / -type f -size 1033c ! -executable ./directoryname/.file2
 #using server info
 find /-user [Primaery_user] -group [group_name]  -size [size]c  -type f 2>/dev/null
+
+#grebbing the specific word using LNX Cmds 
+
+cat data.txt | grep -w millionth | awk $1 '{print $2}'
+
+#find the unique string in the bunch of strings lines that occur only once at time
+#sort the bunch
+cat filename.txt | sort | uniq -c
