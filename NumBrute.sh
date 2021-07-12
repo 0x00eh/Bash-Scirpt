bb764ddd42960ec956b2bda4c6d24f5f66478e3d
Theblackthreat <57785831+theblackthreat@users.noreply.github.com>
2021-07-12 09:12:56 +0530

    Create NumBrute.sh

---
+++ NumBrute.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Creating a file that can brute-force/bypass the pin-code checking Point 
+#creating a list of number which is aquired for the passowrd both at one time.
+#if password pin is 6-digit length.
+password = $(your_password_which you are authenticating)
+for i in {000000..999999}
+do 
+   echo " $password $i"
+done
+
